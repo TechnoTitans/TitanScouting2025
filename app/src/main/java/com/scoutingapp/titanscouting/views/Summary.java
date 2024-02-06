533fe6ac3e4cc0f1a07a69af5779d0972e08ddda
Zizhan Wang <zizwang2560@gmail.com>
2024-02-06 16:41:44 -0500

    summary complete

--- app/src/main/java/com/scoutingapp/titanscouting/views/Summary.java
+++ app/src/main/java/com/scoutingapp/titanscouting/views/Summary.java
@@ -2,7 +2,9 @@ package com.scoutingapp.titanscouting.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.scoutingapp.titanscouting.R;
 
@@ -13,4 +15,16 @@ public class Summary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
     }
+
+//    public void teleOp(View v){
+//        Intent i = new Intent(this, Teleop.class);
+//        i.putExtra("matchNumber", match.getMatchNum());
+//        startActivity(i);
+//    }
+//
+//    public void preGame(View v){
+//        Intent i = new Intent(this, Pregame.class);
+//        i.putExtra("matchNumber", match.getMatchNum());
+//        startActivity(i);
+//    }
 }
