ae3329237554f4d42f6e936f1c2e959c63b15744
Dinofish32 <akarshd32@gmail.com>
2024-02-20 19:32:15 -0500

    initial qr commit

---
+++ app/src/main/java/com/scoutingapp/titanscouting/QRScreen.java
@@ -0,0 +1,14 @@
+package com.scoutingapp.titanscouting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QRScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrscreen);
+    }
+}
