b3c76993285dbe5da637ce979ebe59af1584a158
ayushpatwari <ayushvpatwari@gmail.com>
2024-03-04 16:28:07 -0500

    only endgame and qr need to be done

--- app/src/main/java/com/scoutingapp/titanscouting/views/Endgame.java
+++ app/src/main/java/com/scoutingapp/titanscouting/views/Endgame.java
@@ -2,14 +2,24 @@ package com.scoutingapp.titanscouting.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.ToggleButton;
 
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.scoutingapp.titanscouting.R;
@@ -29,209 +39,123 @@ public class Endgame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_endgame);
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.stagePostions,
+                android.R.layout.simple_spinner_item
+        );
 
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-//                this,
-//                R.array.stagePostions,
-//                android.R.layout.simple_spinner_item
-//        );
-//
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        ((Spinner) (findViewById(R.id.stagePositionSpinner))).setAdapter(adapter);
-//
-//        matchViewModel = new ViewModelProvider(this).get(MatchViewModel.class);
-//        matchViewModel.getMatch(getIntent().getIntExtra("matchNumber", 0)).observe(this, match -> {
-//            this.match = match;
-//            ((Spinner) (findViewById(R.id.stagePositionSpinner))).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//                @Override
-//                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                    match.setStagePosition((String) parent.getItemAtPosition(position));
-//                }
-//
-//                @Override
-//                public void onNothingSelected(AdapterView<?> parent) {
-//
-//                }
-//            });
-//        });
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-//        View backButton = findViewById(R.id.Back);
-//        View nextButton = findViewById(R.id.Next);
+        ((Spinner) (findViewById(R.id.stagePositionSpinner))).setAdapter(adapter);
 
-//        backButton.setOnClickListener(v -> {
-//            Intent i = new Intent(Endgame.this, Teleop.class);
-//            i.putExtra("matchNumber", match.getMatchNum());
-//            startActivity(i);
-//        });
-//
-//        nextButton.setOnClickListener(v -> {
-//            Intent i = new Intent(Endgame.this, Summary.class);
-//            i.putExtra("matchNumber", match.getMatchNum());
-//            startActivity(i);
-//        });
+        matchViewModel = new ViewModelProvider(this).get(MatchViewModel.class);
+        matchViewModel.getMatch(getIntent().getIntExtra("matchNumber", 0)).observe(this, match -> {
+            this.match = match;
+            ((Spinner) (findViewById(R.id.stagePositionSpinner))).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    match.setStagePosition((String) parent.getItemAtPosition(position));
+                    matchViewModel.addMatchInformation(match);
+                }
 
-//        ((Button) (findViewById(R.id.trap))).setOnClickListener(v -> {
-//            Log.d("trapped", "trapped");
-//        });
-//
-//        ((Button) (findViewById(R.id.attempt_trap))).setOnClickListener(v -> {
-//            Log.d("attempted trapped", "attempted trapped");
-//        });
-//
-//        ((ToggleButton) (findViewById(R.id.park_button))).setOnClickListener(v -> {
-//            Log.d("parked", "parked");
-//        });
-//
-//        ((Button) (findViewById(R.id.attempt_onstage))).setOnClickListener(v -> {
-//            Log.d("attempted onstage", "attempted onstage");
-//        });
-//        ((Button) (findViewById(R.id.Onstage))).setOnClickListener(v -> {
-//            Log.d("onstage", "onstage");
-//        });
-//        ((Button) (findViewById(R.id.attempt_harmony))).setOnClickListener(v -> {
-//            Log.d("attempted harmony", "attempted harmony");
-//        });
-//        ((Button) (findViewById(R.id.harmony_button))).setOnClickListener(v -> {
-//            Log.d("harmony", "harmony");
-//        });
-//        ((ToggleButton) (findViewById(R.id.drop_pieces_button))).setOnClickListener(v -> {
-//            Log.d("dropped pieces", "dropped pieces");
-//        });
-//        ((Button) (findViewById(R.id.human_attempt))).setOnClickListener(v -> {
-//            Log.d("human attempted", "human attempted");
-//        });
-//        ((Button) (findViewById(R.id.human_made))).setOnClickListener(v -> {
-//            Log.d("human made", "human made");
-//        });
-//        ((ToggleButton) (findViewById(R.id.collaboration_button))).setOnClickListener(v -> {
-//            Log.d("collaboration done", "collaboration done");
-//        });
-//        ((ToggleButton) (findViewById(R.id.pick_up_from_ground_button))).setOnClickListener(v -> {
-//            Log.d("picked up from ground", "picked up from ground");
-//        });
-//        Log.d("can pick up from ground", "can pick up from ground");
-//
-//        RatingBar driver_quality_bar = (RatingBar) findViewById(R.id.driver_quality_bar);
-//        int numberOfStars = driver_quality_bar.getNumStars();
-//
-//        Log.d(String.valueOf(numberOfStars), String.valueOf(numberOfStars));
-//        RatingBar defense_bar = (RatingBar) findViewById(R.id.defense_bar);
-//        int numberOfStars1 = driver_quality_bar.getNumStars();
-//        Log.d(String.valueOf(numberOfStars1), String.valueOf(numberOfStars1));
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+            ((CheckBox) (findViewById(R.id.noteInTrapCheckBox))).setOnClickListener(v -> {
+                match.setNoteInTrapScored(!match.isNoteInTrapScored());
+                matchViewModel.addMatchInformation(match);
+            });
+
+            ((CheckBox) (findViewById(R.id.disqualifiedCheckBox))).setOnClickListener(v -> {
+                match.setDisqualified(!match.isDisqualified());
+                matchViewModel.addMatchInformation(match);
+            });
+
+            ((RatingBar) (findViewById(R.id.driverQualityRatingBar))).setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    match.setDriverQuality((int) (rating));
+                    matchViewModel.addMatchInformation(match);
+                }
+            });
+
+            ((CheckBox) (findViewById(R.id.dropsPiecesOftenCheckBox))).setOnClickListener(v -> {
+                match.setDropsPiecesOften(!match.isDropsPiecesOften());
+                matchViewModel.addMatchInformation(match);
+            });
+
+            ((CheckBox) (findViewById(R.id.pickRingsFromGroundCheckBox))).setOnClickListener(v -> {
+                match.setPickRingsFromGround(!match.isPickRingsFromGround());
+                matchViewModel.addMatchInformation(match);
+            });
+
+            ((CheckBox) (findViewById(R.id.penaltiesIncurredCheckBox))).setOnClickListener(v -> {
+                match.setPenaltiesIncured(!match.isPenaltiesIncured());
+                matchViewModel.addMatchInformation(match);
+            });
+
+            ((RatingBar) (findViewById(R.id.defenseAbilityRatingBar))).setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    match.setDefenseAbility((int) (rating));
+                    matchViewModel.addMatchInformation(match);
+                }
+            });
+
+            ((RatingBar) (findViewById(R.id.mechanicalReliabilityRatingBar))).setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    match.setMechanicalReliability((int) (rating));
+                    matchViewModel.addMatchInformation(match);
+                }
+            });
+
+            ((EditText) (findViewById(R.id.commentsEditText))).addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    match.setNotes(s.toString());
+                    matchViewModel.addMatchInformation(match);
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+
+
+            ((RatingBar) (findViewById(R.id.driverQualityRatingBar))).setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    match.setDriverQuality((int) (rating));
+                    matchViewModel.addMatchInformation(match);
+                }
+            });
+        });
+
+
+        View backButton = findViewById(R.id.backButton);
+        View nextButton = findViewById(R.id.nextButton);
+        backButton.setOnClickListener(v -> {
+            Intent i = new Intent(Endgame.this, Teleop.class);
+            i.putExtra("matchNumber", match.getMatchNum());
+            startActivity(i);
+        });
+
+        nextButton.setOnClickListener(v -> {
+            Intent i = new Intent(Endgame.this, Summary.class);
+            i.putExtra("matchNumber", match.getMatchNum());
+            startActivity(i);
+        });
+
     }
-
-//    public void scoredInTrap(View v){
-//        ToggleButton button = (ToggleButton) (findViewById(R.id.toggleButton));
-//        String state = button.getText().toString();
-//
-//        if (state.equals("off")){
-//            match.setNoteInTrapScored(true);
-//        } else if (state.equals("on")){
-//            match.setNoteInTrapScored(false);
-//        }
-//    }
-//
-//    public void disqualified(View v){
-//        ToggleButton button = (ToggleButton) (findViewById(R.id.toggleButton2));
-//        String state = button.getText().toString();
-//
-//        if (state.equals("off")){
-//            match.setDisqualified(true);
-//        } else if (state.equals("on")){
-//            match.setDisqualified(false);
-//        }
-//    }
-//
-//    public void penaltiesIncurred(View v){
-//        ToggleButton button = (ToggleButton) (findViewById(R.id.toggleButton3));
-//        String state = button.getText().toString();
-//
-//        if (state.equals("off")){
-//            match.setPenaltiesIncured(true);
-//        } else if (state.equals("on")){
-//            match.setPenaltiesIncured(false);
-//        }
-//    }
-//
-////    public void goodCollaboration(View v){
-////        ToggleButton button = (ToggleButton) (findViewById(R.id.toggleButton4));
-////        String state = button.getText().toString();
-////
-////        if (state.equals("off")){
-////            match.setGoodCollaboration(true);
-////        } else if (state.equals("on")){
-////            match.setGoodCollaboration(false);
-////        }
-////    }
-//
-//    public void dropsPieces(View v){
-//        ToggleButton button = (ToggleButton) (findViewById(R.id.dropsPiecesToggle));
-//        String state = button.getText().toString();
-//
-//        if (state.equals("off")){
-//            match.setDropsPiecesOften(true);
-//        } else if (state.equals("on")){
-//            match.setDropsPiecesOften(false);
-//        }
-//    }
-//
-//    public void canPickFromGround(View v){
-//        ToggleButton button = (ToggleButton) (findViewById(R.id.canPickFromGroundToggle));
-//        String state = button.getText().toString();
-//
-//        if (state.equals("off")){
-//            match.setPickRingsFromGround(true);
-//        } else if (state.equals("on")){
-//            match.setPickRingsFromGround(false);
-//        }
-//    }
-//
-////    public void isKitBot(View v){
-////        ToggleButton button = (ToggleButton) (findViewById(R.id.kitBot));
-////        String state = button.getText().toString();
-////
-////        if (state.equals("off")){
-////            match.setKitBot(true);
-////        } else if (state.equals("on")){
-////            match.setKitBot(false);
-////        }
-////    }
-////
-////    public void isPancake(View v){
-////        ToggleButton button = (ToggleButton) (findViewById(R.id.pancake));
-////        String state = button.getText().toString();
-////
-////        if (state.equals("off")){
-////            match.setPancake(true);
-////        } else if (state.equals("on")){
-////            match.setPancake(false);
-////        }
-////    }
-//
-    public void summaryPage(View v){
-//        SeekBar mechanicalBar = (SeekBar) (findViewById(R.id.seekBar1));
-//        int mechanicalPoints = mechanicalBar.getProgress();
-//
-//        match.setMechanicalReliability(mechanicalPoints);
-//
-//        SeekBar defenseBar = (SeekBar) (findViewById(R.id.seekBar2));
-//        int defensePoints = defenseBar.getProgress();
-//
-//        match.setDefenseAbility(defensePoints);
-//
-//        SeekBar driverQualityBar = (SeekBar) (findViewById(R.id.seekBar3));
-//        int driverQualityPoints = driverQualityBar.getProgress();
-//
-//        match.setDriverQuality(driverQualityPoints);
-//
-//        matchViewModel.addMatchInformation(match);
-
-        Intent i = new Intent(this, Summary.class);
-        i.putExtra("matchNumber", matchNum);
-        startActivity(i);
-    }
-
-
-
-
 }
