5471184c2297d30f3c26ef62830f8a061b3d38b1
anpham09 <anpham.09@gmail.com>
2025-02-06 16:44:31 -0500

    comments on Homepage.java

--- app/src/main/java/com/scoutingapp/titanscouting/Homepage.java
+++ app/src/main/java/com/scoutingapp/titanscouting/Homepage.java
@@ -17,7 +17,8 @@ import com.scoutingapp.titanscouting.database.MatchViewModel;
 import com.scoutingapp.titanscouting.views.logs.Logs;
 import com.scoutingapp.titanscouting.views.QRScreen;
 
-// Class holding the homepage of the app.
+
+//Homepage class represents the main screen of the app.
 public class Homepage extends AppCompatActivity {
 
     private MatchViewModel matchViewModel;
@@ -25,30 +26,37 @@ public class Homepage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the xml file for the homepage
         setContentView(R.layout.activity_main);
 
+        // Initialize buttons by findViewById from the xml file
         View pregameButton = findViewById(R.id.button);
         View logsButton = findViewById(R.id.logs_btn);
 
+        // Set onClickListener method for the pregameButton to navigate to Pregame activity
         pregameButton.setOnClickListener(v -> {
             Intent i = new Intent(this, Pregame.class);
             startActivity(i);
         });
+
+        // Set onClickListener method for the logsButton to navigate to Logs activity
         logsButton.setOnClickListener(v -> {
             Intent i = new Intent(this, Logs.class);
             startActivity(i);
         });
 
+        // Initialize matchViewModel to manage match data
         matchViewModel = new ViewModelProvider(this).get(MatchViewModel.class);
 
-//        matchViewModel.deleteAllMatches();
+        // Uncomment the bottom line to delete all matches
+        // matchViewModel.deleteAllMatches();
+
+        // Create an adapter to handle displaying match data
         final MatchListAdapter adapter = new MatchListAdapter(new MatchListAdapter.MatchDiff());
 
+        // Observe the match data in MatchViewModel class and submit the adapter when data changes
         matchViewModel.getAllMatches().observe(this, matches -> {
             adapter.submitList(matches);
         });
-
-
     }
-
 }
