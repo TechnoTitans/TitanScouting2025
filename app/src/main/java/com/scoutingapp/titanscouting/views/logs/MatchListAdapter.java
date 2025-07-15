dac6ccfad0078a4dcd068f4b01e902551959f52f
nelson <nelsonliu777@gmail.com>
2025-07-15 13:59:07 -0400

    qr cod

--- app/src/main/java/com/scoutingapp/titanscouting/views/logs/MatchListAdapter.java
+++ app/src/main/java/com/scoutingapp/titanscouting/views/logs/MatchListAdapter.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 
 import com.scoutingapp.titanscouting.database.Match;
+import com.scoutingapp.titanscouting.views.QRScreen;
 import com.scoutingapp.titanscouting.views.Summary;
 import com.scoutingapp.titanscouting.views.logs.MatchViewHolder;
 
@@ -36,7 +37,7 @@ public class MatchListAdapter extends ListAdapter<Match, MatchViewHolder> {
         holder.getButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(v.getContext(), Summary.class);
+                Intent i = new Intent(v.getContext(), QRScreen.class);
                 i.putExtra("matchNumber", Integer.parseInt(((Button) (holder.getButton())).getText().toString().split(": ")[1]));
                 v.getContext().startActivity(i);
             }
